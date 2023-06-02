fa3775fec7551ddbfab86e507f25ce08aa13c480
DERP-DESKTOP\Nate <natebrks@gmail.com>
2023-06-03 04:48:57 +1000

    Created Practice File 03_07

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,36 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const hockeyStick = {
+  brand: "Bauer",
+  flex: 87,
+  curve: "P92",
+  kick: "Mid-kick",
+  isTaped: true,
+  toggleTaped: function (tapeStatus) {
+    this.isTaped = tapeStatus;
+  },
+};
+
+const hockeyBag = {
+  brand: "Conway & Banks",
+  size: "Adult",
+  color: "Mighty Duck Green",
+  pockets: 2,
+  accessoryBag: {
+    brand: "Conway & Banks",
+    color: "Black",
+    clearTape: true,
+    tapeScissors: true,
+    toggleClear: function (hasClear) {
+      this.clearTape = hasClear;
+    },
+    toggleScissors: function (hasScissors) {
+      this.tapeScissors = hasScissors;
+    },
+  },
+};
+
+console.log(hockeyStick);
+console.log(hockeyBag);
